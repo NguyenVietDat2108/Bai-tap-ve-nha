c50d468b8206c9d2ac9fd9f067af2f8cd8ce5d70
NguyenVietDat2108 <133034110+NguyenVietDat2108@users.noreply.github.com>
2023-05-09 23:08:06 +0700

    homework5.2
    homework5.2

---
+++ homework 5.2/homework 5.2/Employee.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace homework_5._2
+{
+    class Employee
+    {
+        protected int id;
+        protected string name;
+        protected int age;
+        public int Id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        public int Age
+        {
+            get { return age; }
+            set { age = value; }
+        }
+        public Employee(int id,string name,int age)
+        {
+            this.id = id;
+            this.name = name;
+            this.age = age;
+        }
+        public override string ToString()
+        {
+            return $"ID{Id},Name{Name},Age{Age}";
+        }
+    }
+
+    class Department
+    {
+        public string name;
+        public Employee[] employee;
+        public Department(string  name,int n)
+        {
+            this.name = name;
+            employee = new Employee[n];
+        }
+        public string Name
+        {
+            get { return name; }
+            set
+            {
+                if (value == null)
+                    throw new ArgumentNullException();
+                name = value;
+            }
+        }
+        public Employee this[int index]
+        {
+            get
+            {
+                if (index < 0 || index > employee.Length - 1)
+                    return null;
+                return employee[index];
+            }
+            set
+            {
+                if (index < 0 || index > employee.Length - 1)
+                    throw new ArgumentOutOfRangeException();
+                employee[index] = value;
+            }
+        }
+    }
+}
---
+++ homework 5.2/homework 5.2/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace homework_5._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter department's name:");
+            string DepartmentName = Convert.ToString(Console.ReadLine());
+            Console.WriteLine("Enter number of employees:");
+            int EmployeeNumber = Convert.ToInt32(Console.ReadLine());
+            Department a = new Department(DepartmentName,EmployeeNumber);
+
+            for (int i = 1; i <= EmployeeNumber; i++)
+            {
+                Console.WriteLine("Enter id:");
+                int id = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Enter name:");
+                string name = Convert.ToString(Console.ReadLine());
+                Console.WriteLine("Enter age:");
+                int age = Convert.ToInt32(Console.ReadLine());
+                a.employee[i] = new Employee(id, name, age);
+            }
+
+            for (int i = 1; i <= EmployeeNumber; i++)
+            {
+                Console.WriteLine(a.employee[i].ToString());
+                Console.ReadLine();
+            }
+
+        }
+    }
+}
