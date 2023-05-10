4934ba12d9dcbdff5996c7f61a8e7f28ee27281b
NguyenVietDat2108 <133034110+NguyenVietDat2108@users.noreply.github.com>
2023-05-10 23:44:23 +0700

    homework 6.2
    homework 6.2

---
+++ homework 6.2/homework 6.2/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace homework_6._2
+{
+    class InvalidMarkException : Exception
+    {   
+        public InvalidMarkException():base("Wrong number,retry!")
+        {
+        }
+    }
+    class Student
+    {
+        private string id;
+        private string name;
+        private double theorymark;
+        private double labmark;
+        public Student(string id, string name, double theorymark, double labmark)
+        {
+            this.id = id;
+            this.name = name;
+            this.theorymark = theorymark;
+            this.labmark = labmark;
+           
+        } 
+        public void Display()
+        {
+            Console.WriteLine("Info: " + "Id:" + id + " Name:" + name 
+                + " Theorymark:" + theorymark + " Labmark:" + labmark);
+        }
+    }
+    
+    class Program
+    {    
+        static void Main(string[] args)
+        {
+            double d = new double();
+            double e = new double();
+            Console.WriteLine("Enter id:");
+            string b = Convert.ToString(Console.ReadLine());
+            Console.WriteLine("Enter name:");
+            string c = Convert.ToString(Console.ReadLine());
+            Console.WriteLine("Enter theorymark:");
+            try
+            {
+                
+                d = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("Enter labmark:");
+                e = Convert.ToDouble(Console.ReadLine());
+                if (d < 0 || d > 10 || e < 0 || e > 10)
+                {
+                    throw new InvalidMarkException();
+                }
+            }
+            catch(InvalidMarkException invalid)
+            {
+                Console.WriteLine(invalid.Message);
+            }
+            if (d > 0 && d > 10 && e < 0 && e > 10)
+            {
+                Student a = new Student(b, c, d, e);
+                a.Display();
+            }
+        }
+    }
+}
---
+++ homework 6.3/homework 6.3/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace homework_6._3
+{    class AmountException : Exception
+    {
+        public AmountException():base("Problems with salary|bonus")
+        {
+        }
+    }
+    class Lecture
+    {
+        private string name;
+        private double salary;
+        private double bonus;
+        public Lecture(string name,double salary,double bonus)
+        {
+                this.name = name;
+                this.salary = salary;
+                this.bonus = bonus;
+        }
+        public void Display()
+        {
+             Console.WriteLine( "Info:" + "Name:"+ name +" Salary:" + salary +" Bonus:"+ bonus);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double salary = new double() ;
+            double bonus = new double() ;
+            Console.WriteLine("Enter name:");
+            string name = Convert.ToString(Console.ReadLine());
+            try
+            {
+                Console.WriteLine("Enter salary($):");
+                salary = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("Enter bonus($):");
+                bonus = Convert.ToDouble(Console.ReadLine());
+                
+                if (salary < 60000 | bonus > 10000)
+                {
+                    throw new AmountException();
+                }
+            }
+            catch (AmountException exception)
+            {
+                Console.WriteLine(exception.Message);
+            }
+            if (salary > 60000 && bonus < 10000)
+            {
+            Lecture a = new Lecture(name, salary, bonus);
+            a.Display();
+            }
+        }
+    }
+}
