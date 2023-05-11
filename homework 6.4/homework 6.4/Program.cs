3e76328d5b2f83425a0e8d6a5b9aad60f54d0868
NguyenVietDat2108 <133034110+NguyenVietDat2108@users.noreply.github.com>
2023-05-12 02:36:11 +0700

    homework 6.4 and 6.5
    homework 6.4 and 6.5

---
+++ homework 6.4/homework 6.4/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace homework_6._4
+{
+    delegate void IntAction(int a);
+    class TestDelegate
+    {
+        public static void PrintInt(int a)
+        {
+            Console.WriteLine("Print interger:" + a);
+        }
+        public static void Peform(IntAction act,int[]arr)
+        {
+            Console.WriteLine("")
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IntAction act;
+            act(42);
+        }
+    }
+}
---
+++ homework 6.5/homework 6.5/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace homework_6._5
+{
+    class Employee
+    {
+        internal int EmpNo;
+        internal string Name;
+        internal string Email;
+        internal int Phone;
+        public Employee(int EmpNo, string Name, string Email, int Phone)
+        {
+            this.EmpNo = EmpNo;
+            this.Name = Name;
+            this.Email = Email;
+            this.Phone = Phone;
+        }
+    }
+    class Company
+    { 
+        internal string Name;
+        public Company(string Name)
+        {
+           this.Name = Name;
+        }
+    }
+    delegate void Simultaneously();
+    class Program
+    {
+        event Simultaneously Add;
+        public static void ErrorInput()
+        {
+            Console.WriteLine("Error Input");
+        }
+        public static void Sucess()
+        {
+            Console.WriteLine("Sucess");
+        }
+        static void Main(string[] args)
+        {
+            int kiemtra = 1;
+
+            Console.WriteLine("Enter number of employees:");
+            int number = Convert.ToInt32(Console.ReadLine());
+            Employee[] a = new Employee[number];
+            for (int i = 0; i <= number; i++)
+            {
+                Console.WriteLine("Enter name:");
+            }
+            for (int i = 0; i <= number; i++)
+            {
+                for (int j = 0; j <i; j++)
+                {
+                    if (a[i] == a[j])
+                    {
+                        kiemtra = 0;
+                        ErrorInput();
+                    }                
+                }
+            }
+
+            if(kiemtra==1)
+            {
+                Sucess();
+            }
+
+            
+        }
+            
+
+
+        }
+    }
+
