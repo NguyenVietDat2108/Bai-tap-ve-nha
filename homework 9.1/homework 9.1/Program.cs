f164b5230d9e09046700814979a4903af7c1c683
NguyenVietDat2108 <133034110+NguyenVietDat2108@users.noreply.github.com>
2023-05-22 23:34:52 +0700

    ex 9.5,homework 9.1 and 9.2

---
+++ homework 9.1/homework 9.1/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace homework_9._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter number:");
+            int number = Convert.ToInt32(Console.ReadLine());
+            Func<int,string> checkprime=delegate (int number1)
+            {
+                int a = 1;
+                string b = "La so nguyen to";
+                for(int i=2; i<number1; i++)
+                {
+                    if (number < 2) 
+                    {
+                        a=0; break;
+                    }
+                    
+                    if(number1%i==0)
+                    {
+                        a =0;break;
+                    }
+                }
+                if (a == 0)
+                    b = "Khong phai so nguyen to";
+                return b;
+            };
+            Console.WriteLine(checkprime(number)) ;
+
+            Console.WriteLine("Enter number1:");
+            int n= Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter number2:");
+            int m= Convert.ToInt32(Console.ReadLine());
+
+            Func<int,int, int> sum = delegate (int number1,int number2)
+            {
+                return (number1 + number2) / 2 * (number2 - number1 + 1);
+            };
+            Console.WriteLine("Sum from n to m:"+sum(n,m));
+        }
+    }
+}
---
+++ homework 9.2/homework 9.2/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace homework_9._2
+{
+    class Program
+    {
+        public static class ExtensionMethod<T>
+        {
+            public static void Checkdifference(List<string> a,int number)
+            {
+                for(int i=0;i<number;i++)
+                {
+                    for(int j=i+1;j<number;j++)
+                    {
+                        if (a[j] == a[i])
+                        {
+                            a.RemoveAt(j);
+                            number = number - 1;
+                        }
+                    }
+                }
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the number of elements:");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            List <string> list = new List<string>();
+            for(int i=0;i<number;i++)
+            {
+                Console.WriteLine("Enter name:");
+                list.Add(Convert.ToString(Console.ReadLine()));
+            }
+
+            ExtensionMethod<string>.Checkdifference(list,number);
+
+            foreach(var a in list)
+            {
+                Console.WriteLine(a);
+            }
+        }
+    }
+}
