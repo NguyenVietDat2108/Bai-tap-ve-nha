0ac00e9284d9d80049ae57927a8a5082284505b7
NguyenVietDat2108 <133034110+NguyenVietDat2108@users.noreply.github.com>
2023-05-24 23:42:09 +0700

    ex 10.5 and 10.6

---
+++ ex 10.4/ex 10.4/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ex_10._4
+{
+    class Film
+    {
+        public string FilmId { get; set; }
+        public string FilmName { get; set; }
+        public int Price { get; set; }
+        public override string ToString()
+        {
+            return FilmId + ":" + ":" + Price;
+        }
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                int[] Numbers = { 7, 9, 3, 5, 2, 1, 0, 6, 4, 3, 1 };
+                string[] Words =
+                {"Chi","trich","phe", "phan", "nguoi","khac",
+                    "giong","nhu","con","chim","bo","cau","dua","thu",
+                    "bao", "gio","cung", "quay","ve","noi","xuat", "phat"
+};
+                List<Film> ListFilm = new List<Film>()
+{
+                new Film{FilmId="F01",FilmName="Diep vien 007",Price=120000},
+                new Film{FilmId="F02",FilmName="Tam quoc dien nghia",Price=130000},
+                new Film{FilmId="F03",FilmName="Thieu lam truyen ky",Price=16000},
+                new Film{FilmId="F04",FilmName="Nguoi nhen 2",Price=100000},
+                new Film{FilmId="F05",FilmName="Ngan hang tinh yeu",Price=340000},
+                new Film{FilmId="F06",FilmName="Nguoi dep va quai thu",Price=230000},
+                new Film{FilmId="F07",FilmName="Biet dong Sai Gon",Price=190000},
+};
+                IEnumerable<int> querynumber = Numbers.Where(n => n % 2 == 0);
+                Show<int>(querynumber, "Loc cac so chan:");
+
+                IEnumerable<string> queryword = Words.Where(w => w.Length > 4);
+                Show<string>(queryword, "Loc cac tu co do dai >4:");
+
+                IEnumerable<string> queryT = Words.Where(w => w.StartsWith("t"));
+                Show<string>(queryT, "Loc cac tu co ten bat dau bang chu t:");
+
+                var uniqueNumber = Numbers.Distinct();
+                Show<int>(uniqueNumber, "Loc cac so duy nhat trong tap cac so:");
+
+                var countDistinct = Words.Distinct().Count();
+                Console.WriteLine("Dem xem co bao nhieu tu khong trung nhau:" +
+                countDistinct);
+
+                var fourNumber = Numbers.Take(4);
+                Show<int>(fourNumber, "Lay 4 so dau tien trong day:");
+
+                var twoword = Words.Take(2);
+                Show<string>(twoword, "Lay 2 tu dau tien trong cau:");
+
+                var searchword = Words.TakeWhile(w => w.Contains('t'));
+                Show<string>(searchword, "Lay nhung tu dau tien co chua chu t:");
+
+                var queryfilm = ListFilm.OrderBy(f => f.Price)
+                .Select(x => new { x.FilmId, x.FilmName, x.Price })
+                .ToList().TakeWhile(t => t.Price < 200000);
+
+                var skipNumber = Numbers.Skip(3);
+                Show<int>(skipNumber, "Bo qua 3 phan tu dau tien, lay tat ca cac phan tu con lai: ");
+                
+                var skipTakeNumber = Numbers.Skip(4).Take(3);
+                Show<int>(skipTakeNumber, "Bo qua 4 phan tu dau tien, lay 3 phan tu ke tiep: ");
+                
+                var skipTakeFilm = ListFilm.Skip(3).Take(3);
+                Show<Film>(skipTakeFilm, "Bo qua 3 phim dau tien, lay 3 phim ke tiep: ");
+                
+                var sortNumber = Numbers.OrderByDescending(x => x).SkipWhile(x => x> 5);
+                Show<int>(sortNumber, "Sap xep giam dan, sau do lay cac phan tu < 5:");
+}
+
+            static void Show<T>(IEnumerable<T> data, string message)
+            {
+                Console.WriteLine(message);
+                foreach (var item in data)
+                {
+                    Console.WriteLine(item);
+                }
+            }
+        }
+    },
+            }
+        }
+    }
+}
---
+++ ex 10.5/ex 10.5/Program.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ex_10._5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            static void Main(string[] args)
+            {
+                HRMContainer hrm = new HRMContainer();
+                Department dep1 = new Department()
+                {
+                    DepId = "D01",
+                    DepName = "Phong Hanh Chinh" };
+                Department dep2 = new Department()
+                {
+                    DepId = "D02",
+                    DepName = "Phong Nhan Su" };
+                Department dep3 = new Department()
+                {
+                    DepId = "D03",
+                    DepName = "Phong Ky Thuat" };
+                }
+            Department dep4 = new Department()
+            {
+                DepId = "D04",
+                DepName = "Phong Marketing" };
+            hrm.Departments.Add(dep1);
+            hrm.Departments.Add(dep2);
+            hrm.Departments.Add(dep3);
+            hrm.Departments.Add(dep4);
+
+            Employee emp1 = new Employee()
+            {
+                EmId = "E01",
+                FirstName = "Tran Van",
+                LastName = "Tuan",
+                Email = "tuan@gmail.com",
+                Phone = "0977689877",
+                Sex = true,
+                BirthDay = new DateTime(1990, 1, 20),
+                DepId = "D01"
+            };
+            Employee emp2 = new Employee()
+            {
+                EmId = "E02",
+                FirstName = "Hoang Van",
+                LastName = "Hoa",
+                Email = "hoa@gmail.com",
+                Phone = "0977689877",
+                Sex = true,
+                BirthDay = new DateTime(1990, 1, 20),
+                DepId = "D01"
+            };
+            Employee emp3 = new Employee()
+            {
+                EmId = "E03",
+                FirstName = "Le Van",
+                LastName = "Hai",
+                Email = "hai@gmail.com",
+                Phone = "0977689877",
+                Sex = true,
+                BirthDay = new DateTime(1990, 1, 20),
+                DepId = "D02"
+            };
+            Employee emp4 = new Employee()
+            {
+                EmId = "E04",
+                FirstName = "Nguyen Van",
+                LastName = "Long",
+                Email = "long@gmail.com",
+                Phone = "0977689877",
+                Sex = true,
+                BirthDay = new DateTime(1990, 1, 20),
+                DepId = "D02"
+            };
+            Employee emp5 = new Employee()
+            {
+                EmId = "E05",
+                FirstName = "Ho Van",
+                LastName = "Hanh",
+                Email = "hanh@gmail.com",
+                Phone = "0977689877",
+                Sex = true,
+                BirthDay = new DateTime(1990, 1, 20),
+                DepId = "D02"
+            };
+
+            hrm.Employees.Add(emp1);
+            hrm.Employees.Add(emp2);
+            hrm.Employees.Add(emp3);
+            hrm.Employees.Add(emp4);
+            hrm.Employees.Add(emp5);
+
+            hrm.SaveChanges();
+
+            var listE = from e in hrm.Employees select e;
+            Console.WriteLine("Hien thi danh sach nhan vien:");
+            foreach (var item in listE)
+            {
+                Console.WriteLine(item.FirstName + " " + item.LastName + "- Phong:"
+                + item.Department.DepName);
+            }
+
+            var listH = from e in hrm.Employees
+                        where e.LastName.StartsWith("H")
+                        select e;
+            Console.WriteLine("Hien thi danh sach nhan vien co ten bat dau bang chu H: ");
+            
+            foreach (var item in listH)
+            {
+                Console.WriteLine(item.FirstName + " " + item.LastName);
+            }
+
+            var listA = hrm.Employees.Where(x => x.LastName.Contains("a"));
+            Console.WriteLine("Hien thi danh sach nhan vien co ten chua chu a:");
+            
+            foreach (var item in listA)
+            {
+                Console.WriteLine(item.FirstName + " " + item.LastName);
+            }
+
+            var list3 = hrm.Employees.Take(3);
+            Console.WriteLine("Hien thi danh sach 3 nhan vien dau tien:");
+            foreach (var item in list3)
+            {
+                Console.WriteLine(item.FirstName + " " + item.LastName);
+            }
+        }
+    }
+}
