a30766cd61c75b2bb9a7fc2155ee0e7812df856f
NguyenVietDat2108 <133034110+NguyenVietDat2108@users.noreply.github.com>
2023-05-27 00:24:11 +0700

    ex 10.8 and 10.9

---
+++ ex 10.8/ex 10.8/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ex_10._8
+{
+    class Program
+    {
+            static void Print()
+            {
+                Console.WriteLine("Thuc thi nhiem vu");
+            }
+            static void Main(string[] args)
+            {
+            Task t1 = new Task(new Action(Print));
+
+            t1.Start();
+
+            Task t2 = Task.Run(() => Print());
+
+            t1.Wait();
+            t2.Wait();
+
+            Console.WriteLine("Thead main thuc thi xong");
+        }
+    }
+}
+
---
+++ ex 10.9/ex 10.9/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace ex_10._9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\nQuan sat ket qua lap truyen thong");
+            for (int i = 0; i <= 10; i++)
+            {
+                Console.WriteLine("i = {0} executed by thread with ID {1}", i, Thread.CurrentThread.ManagedThreadId);
+                Thread.Sleep(200);
+            }
+            Console.WriteLine("\nQuan sat ket qua lap song song");
+
+            System.Threading.Tasks.Parallel.For(0, 10, i =>
+            {
+                Console.WriteLine("i = {0} executed by thread with ID {1}",
+                i, Thread.CurrentThread.ManagedThreadId);
+                Thread.Sleep(200);
+            });
+        }
+    }
+    }
+
