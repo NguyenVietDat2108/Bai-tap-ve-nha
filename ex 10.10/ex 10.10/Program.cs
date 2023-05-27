0c2c0ebb093de85edf942e42a6bc84dfb4bd3fb0
NguyenVietDat2108 <133034110+NguyenVietDat2108@users.noreply.github.com>
2023-05-28 00:06:13 +0700

    ex 10.10

---
+++ ex 10.10/ex 10.10/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+         static void Main(string[] args)
+        {
+            string[] words = { "viec", "tuy", "nho", "khong", "lam", "khong",
+                               "xong", "duong", "tuy", "ngan", "khong", "di", "khong", "den" };
+
+            Console.WriteLine("Truy van thong thuong nhan du lieu cham");
+
+            IEnumerable<string> resultNormal = from w in words
+                                               select w.ToUpper();
+            foreach (var r in resultNormal)
+            {
+                Console.Write(r + " ");
+            }
+            Console.WriteLine("\nTruy van song song nhan du lieu nhanh");
+
+            //It only makes sense when we're dealing with LINQ to Objects, where we're dealing with 
+            //data to memory.
+            IEnumerable<string> resultParallel = from wp in words.AsParallel()
+                                                 select wp.ToUpper();
+            foreach (var r in resultParallel)
+            {
+                Console.Write(r + " ");
+            }
+        }
+        }
+    }
+
+
