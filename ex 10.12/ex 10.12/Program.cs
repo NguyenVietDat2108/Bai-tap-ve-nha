9bbfd2ec433a7d9bea8982dee46db18636debda0
NguyenVietDat2108 <133034110+NguyenVietDat2108@users.noreply.github.com>
2023-05-29 00:17:08 +0700

    ex 10.12,10.13 and 10.14

---
+++ ex 10.12/ex 10.12/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace ex_10._12
+{
+    class Program
+    {
+        static async void PerformComplexTaskAsync()
+        {
+            Console.WriteLine("Chuan bi goi phuong thuc bat dong bo");
+            int result = await new ComplexTask().AnalyzeData();
+            Console.WriteLine(result.ToString());
+        }
+        static void Main(string[] args)
+        {
+            PerformComplexTaskAsync();
+            Console.WriteLine("Thread Main executing...");
+            Console.ReadLine();
+        }
+    }
+    class ComplexTask
+    {
+        public Task<int> AnalyzeData()
+        {
+            Task<int> task = new Task<int>(GetResult);
+            task.Start();
+            return task;
+        }
+
+        public int GetResult()
+        {
+            Thread.Sleep(5000);
+            return new Random().Next(1, 1000);
+        }
+    }
+}
---
+++ ex 10.13/ex 10.13/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ex_10._13
+{
+    class Program
+    {
+        static dynamic DynamicMethod(dynamic param)
+        {
+            if (param is int)
+            {
+                Console.WriteLine("Tham so dong kieu int co gia tri {0}",param);
+                return param;
+            }
+            else if (param is string)
+            {
+                Console.WriteLine("Tham so dong kieu string co gia tri {0}",param);
+                return param;
+            }
+            else
+            {
+                Console.WriteLine("Tham so dong kieu khong ro co gia tri {0}", param);
+            return param;
+            }
+        }
+        static void Main(string[] args)
+        {
+            dynamic dynaVar1 = DynamicMethod(3);
+            dynamic dynaVar2 = DynamicMethod("Hello World");
+            dynamic dynaVar3 = DynamicMethod(12.5);
+            Console.WriteLine("\nCac gia tri nha duoc:\n{0} \n{1} \n{2}",
+            dynaVar1, dynaVar2, dynaVar3);
+        }
+    }
+}
